c5fe526f74d0dce85584c3004409b99cf6f49eee
dev global <merong@gmail.com>
2022-08-12 16:54:11 +0900

    Initial Commit

---
+++ TMP/CCBLK_Main.c
@@ -0,0 +1,18 @@
+#include "CCBLK_Inc.h"
+
+/*=========================== Function Header ==============================
+ * name  : main
+ * desc  : Start point
+ * return: CCBLK_rOk, or 
+ * param : None
+ * author: AUTHOR_NAME
+=======================================================================*/
+int main( int argc, char *argv[] )
+{
+	argc = argc;
+	argv = argv;
+
+	return 0;
+
+}
+
