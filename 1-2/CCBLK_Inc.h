578e1a8775befa40bd344f31655d4ca1ce3499cc
dev global <merong@gmail.com>
2022-08-16 08:47:29 +0900

    .

---
+++ 1-2/CCBLK_Inc.h
@@ -0,0 +1,16 @@
+#ifndef _CCBLK_INC_H_
+#define _CCBLK_INC_H_
+
+
+#include <sys/poll.h>
+#include <string.h>
+#include <time.h>
+#include <stdarg.h>
+#include <arpa/inet.h>
+#include <ctype.h>
+
+/* FRAMEWORK */
+#include <dal.h>
+
+
+#endif /*_CCBLK_INC_H_*/
