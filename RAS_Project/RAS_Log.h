318132d409de38bf1cf1e8f13057eed936a42c2c
dev global <ebdhkim@gmail.com>
2023-01-05 12:41:16 +0900

    .

--- RAS_Project/RAS_Log.h
+++
@@ -1,7 +0,0 @@
-/* RAS_Log.h */
-#ifndef _RAS_LOG_H_
-#define _RAS_LOG_H_
-
-int LOG_Init();
-
-#endif /* _RAS_LOG_H_ */
--- RAS_Project/RAS_Util.h
+++
@@ -1,9 +0,0 @@
-/* RAS_Util.h */
-#ifndef _RAS_UTIL_H_
-#define _RAS_UTIL_H_
-
-int UTIL_CheckClientIp( char *pszIp );
-int UTIL_CheckPath( char *pszPath );
-int UTIL_GetIdFromPath( char *pszPath, int *pnId );
-
-#endif /* _RAS_UTIL_H_ */
